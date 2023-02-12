d1782a2583b1c4f357e0a255eb63d8f71109de7e
alexandraminyov29 <alexandraminyov773@gmail.com>
2023-02-13 00:22:41 +0200

    Initial commit - SwiftUI

---
+++ BabyDev/BabyDev/BabyDevApp.swift
@@ -0,0 +1,17 @@
+//
+//  BabyDevApp.swift
+//  BabyDev
+//
+//  Created by Alexandra Minyov on 13.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct BabyDevApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
